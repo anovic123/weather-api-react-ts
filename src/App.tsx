d33acdf0aca141d2949144b7c68264b8ab75ac63
anovic123 <105565287+anovic123@users.noreply.github.com>
2022-12-09 16:54:07 +0300

    add useForecast hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,32 @@
-import { useState, ChangeEvent } from 'react';
-import { optionType } from './types';
+import Search from './components/Search';
+import useForecast from './hooks/useForecast';
 
 const App = (): JSX.Element => {
-  const [term, setTerm] = useState<string>('');
-  const [options, setOptions] = useState<[]>([]);
 
-  const getSearchOptions = (value: string) => {
-    fetch(
-      `http://api.openweathermap.org/geo/1.0/direct?q=${value.trim()},&limit=${5}&appid=${
-        process.env.REACT_APP_API_KEY
-      }`,
-    )
-      .then((res) => res.json())
-      .then((data) => setOptions(data));
-  };
-
-  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const value = e.target.value.trim();
-    setTerm(value);
-
-    if (value === '') return;
-
-    getSearchOptions(value);
-  };
-
-  const onOptionSelect = (option: optionType) => {
-  }
-
+  const {
+    term,
+    options,
+    forecast,
+    onInputChange,
+    onOptionSelect,
+    onSubmit
+  } = useForecast()
   return (
     <main
       className="flex justify-center items-center bg-gradient-to-br
         from-sky-400 via-rose-400 to-lime-400 h-[100vh] w-full"
     >
-      <section
-        className="w-full md:max-w-[500px] p-4 flex flex-col text-center
-          items-center justify-center md:px-10 lg:p-24 h-full lg:h-[500px] bg-white
-          bg-opacity-20 backgrop-blur-ls rounded drop-shadow-lg text-zinc-700"
-      >
-        <h1 className="text-4xl font-thin">
-          Прогноз <span className="font-black">Погоды</span>
-        </h1>
-        <p className="text-sm mt-2">
-          Введите ниже место, где вы хотите узнать погоду, и выберите вариант из выпадающего списка.
-        </p>
-
-        <div className="relative flex mt-10 md:mt-4">
-          <input
-            type="text"
-            value={term}
-            onChange={onInputChange}
-            className="px-2 py-1 rounded-l-md border-2 border-white"
-          />
-
-          <ul className="absolute top-9 bg-white ml-1 rounded-b-md">
-            {options.map((option: optionType, index: number ) => (
-              <li key={option.name + '-' + index}>
-                <button className="text-left text-sm w-full hover:bg-zinc-700
-                  hover:text-white px-2 py-1 cursor-pointer"
-                >
-                  {option.name}
-                </button>
-              </li>
-            ))}
-          </ul>
-
-          <button
-            className="rounded-r-md border-2 border-zinc-100 hover:border-zinc-500
-            hover:text-zinc-500 text-zinc-100 px-2 py-1 cursor-pointer"
-          >
-            Найти
-          </button>
-        </div>
-      </section>
+      {forecast ? (
+        'we have a forecast'
+      ) : (
+        <Search
+          term={term}
+          options={options}
+          onInputChange={onInputChange}
+          onOptionSelect={onOptionSelect}
+          onSubmit={onSubmit}
+        />
+      )}
     </main>
   );
 };
